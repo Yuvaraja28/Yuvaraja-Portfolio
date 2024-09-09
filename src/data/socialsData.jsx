26f1c7a9a6407f2d1829b2a03d31037e06ed3a70
yuvaraja28 <yuvaraja28042005@gmail.com>
2024-09-10 01:29:56 +0530

    Some Changes with Scroll Animation and Datas

--- src/data/socialsData.jsx
+++ src/data/socialsData.jsx
@@ -1,17 +1,22 @@
-import { SiLeetcode, SiUpwork } from "react-icons/si";
+import { SiCodechef, SiLeetcode, SiUpwork } from "react-icons/si";
 import { FaYoutube, FaGithub, FaLinkedin, FaDiscord, FaInstagram, FaPaypal, FaTwitter, FaGoogle } from "react-icons/fa";
 
 const socialsData = [
   {
     name: 'Github',
     comp: <FaGithub size={24} />,
-    url: 'https://github.com/yuvaraja28'
+    url: 'https://github.com/yuvaraja28',
+    footer: true
   },
   {
-    name: 'Upwork',
-    comp: <SiUpwork size={24} />,
-    url: 'https://upwork.com/freelancers/~01cab23a6f7f2b4627',
-    footer: true
+    name: 'LeetCode',
+    comp: <SiLeetcode size={24} />,
+    url: 'https://leetcode.com/u/yuvaraja28/'
+  },
+  {
+    name: 'CodeChef',
+    comp: <SiCodechef size={24} />,
+    url: 'https://www.codechef.com/users/yuvaraja28'
   },
   {
     name: 'Linkedin',
@@ -24,11 +29,6 @@ const socialsData = [
     comp: <FaDiscord size={24} />,
     url: 'https://discord.com/users/759050921413312532',
     footer: true
-  },
-  {
-    name: 'Paypal',
-    comp: <FaPaypal size={24} />,
-    url: 'https://www.paypal.com/paypalme/YuvarajaMurali'
   },
   {
     name: 'Instagram',
@@ -47,15 +47,15 @@ const socialsData = [
     url: 'https://www.youtube.com/channel/UCNwoToYIOA285x9Yg-ChNyw'
   },
   {
-    name: 'Mail',
-    comp: <FaGoogle size={24} />,
-    url: 'mailto:yuv.the.dev@gmail.com',
-    footer: true
+    name: 'Paypal',
+    comp: <FaPaypal size={24} />,
+    url: 'https://www.paypal.com/paypalme/YuvarajaMurali'
   },
   {
-    name: 'LeetCode',
-    comp: <SiLeetcode size={24} />,
-    url: 'https://leetcode.com/u/yuvaraja28/'
+    name: 'Upwork',
+    comp: <SiUpwork size={24} />,
+    url: 'https://upwork.com/freelancers/~01cab23a6f7f2b4627',
+    footer: true
   }
 ]
 
